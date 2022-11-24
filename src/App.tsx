6816c56395018a87b1ca972ada64212e1b6fb547
stephany souza <stephanysr@hotmail.com>
2022-11-24 04:05:25 -0300

    Implementação tema/postagem

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,8 @@ import Home from './paginas/home/Home';
 import Login from './paginas/login/Login';
 import './App.css';
 import CadastroUsuario from './paginas/cadastroUsuario/CadastroUsuario';
-
+import ListaTema from './components/temas/listatema/ListaTema';
+import ListaPostagem from './components/postagens/listaPostagem/ListaPostagem';
 
 function App() {
   return (
@@ -18,6 +19,8 @@ function App() {
           <Route path="/home" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/cadastrousuario" element={<CadastroUsuario/>} />
+          <Route path="/temas" element={<ListaTema/>} />
+          <Route path="/posts" element={<ListaPostagem/>} />
         </Routes>
       </div>
       <Footer />
